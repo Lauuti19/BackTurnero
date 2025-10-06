8b1197b8a51ef18762491dd2c5e600e2d2fb5409
solsv7 <solsv7@gmail.com>
2025-10-06 18:56:47 -0300

    refactor: add middlewares for multiple routes

--- routes/paymentsRoutes.js
+++ routes/paymentsRoutes.js
@@ -5,8 +5,8 @@ const { authenticateToken } = require('../middlewares/authenticateToken');
 const { authorizeRole } = require('../middlewares/authMiddleware');
 
 router.get('/active-fees', authenticateToken, getActiveFees);       
-router.post('/register-fee', authenticateToken, authorizeRole(['admin']),registerFee);      
-router.post('/pay-fee', authenticateToken, authorizeRole(['admin']),payFee);                
+router.post('/register-fee', authenticateToken, authorizeRole(['admin','profesor']),registerFee);      
+router.post('/pay-fee', authenticateToken, authorizeRole(['admin','profesor']),payFee);                
 
 
 module.exports = router;
